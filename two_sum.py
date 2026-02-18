5987952445d54deb4f12a856dae6098ce15fc84c
ashvindrasivakumar <149091591+ashvindrasivakumar@users.noreply.github.com>
2026-02-18 19:17:37 +0530

    Implement twoSum function to find indices of target

---
+++ two_sum.py
@@ -0,0 +1,10 @@
+def twoSum(nums, target):
+    seen = {}
+
+    for i in range(len(nums)):
+        diff = target - nums[i]
+
+        if diff in seen:
+            return [seen[diff], i]
+
+        seen[nums[i]] = i
